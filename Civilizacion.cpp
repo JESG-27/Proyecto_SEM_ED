07b523672ad6eb8870c816d4e935101fa9d6ee5c
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2021-11-03 10:41:52 -0600

    Metodos de la clase Civilizacion

---
+++ Civilizacion.cpp
@@ -0,0 +1,42 @@
+#include "Civilizacion.h"
+
+Civilizacion::Civilizacion()
+{}
+
+Civilizacion::Civilizacion(const string &nombre, const float &ubicacion_x, const float &ubicacion_y, const float &puntuacion)
+{
+    this->nombre = nombre;
+    this->ubicacion_x = ubicacion_x;
+    this->ubicacion_y = ubicacion_y;
+    this->puntuacion = puntuacion;
+}
+
+void Civilizacion::setNombre(const string &civ)
+{
+    nombre=civ;
+}
+
+string Civilizacion::getNombre()
+{
+    return nombre;
+}
+
+void Civilizacion::setUbicacion_x(const float &civ)
+{
+    ubicacion_x = civ;
+}
+
+float Civilizacion::getUbicacion_x()
+{
+    return ubicacion_x;
+}
+
+void Civilizacion::setUbicacion_y(const float &civ)
+{
+    ubicacion_y = civ;
+}
+
+float Civilizacion::getUbicacion_y()
+{
+    return ubicacion_y;
+}
