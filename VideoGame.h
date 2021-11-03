30e4524bd84767ec2e2245fedfa70865905f8b11
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2021-11-03 15:15:23 -0600

    Constructor parametrizado, set y get Nombre

---
+++ VideoGame.h
@@ -0,0 +1,31 @@
+#ifndef VIDEOGAME_H
+#define VIDEOGAME_H
+
+#include "Civilizacion.h"
+#include <vector>
+
+class VideoGame
+{
+    string Username;
+    vector<Civilizacion> Civilizaciones;
+public:
+    VideoGame();
+    VideoGame(const string &nom, const vector<Civilizacion> Civilizaciones);
+    void setNombre(const string &nom);
+    string getNombre();
+    void AgregarCiv(const Civilizacion &civ);
+    void insertar(const Civilizacion &civ, size_t pos);
+    void inicializar(const Civilizacion &civ, size_t n);
+    Civilizacion PrimerCiv();
+    Civilizacion UltimaCiv();
+    void OrdenarNom();
+    void OrdenarUbX();
+    void OrdenarUbY();
+    void OrdenarPun();
+    void eliminar(const string &nom);
+    Civilizacion* buscar(const Civilizacion &civ);
+    size_t size();
+};
+
+
+#endif
