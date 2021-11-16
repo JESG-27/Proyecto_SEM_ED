3ba84e52208c29b8b4d0aff6fda230d764367e72
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2021-11-16 12:53:52 -0600

    Documentación y list de Aldeanos agregado

--- VideoGame.h
+++ VideoGame.h
@@ -2,30 +2,49 @@
 #define VIDEOGAME_H
 
 #include "Civilizacion.h"
+#include "Aldeano.h"
 #include <vector>
+#include <list>
 
 class VideoGame
 {
+private:
     string Username;
     vector<Civilizacion> Civilizaciones;
+    list<Aldeano> aldeanos;
+
 public:
+    // Constructores
     VideoGame();
     VideoGame(const string &nom, const vector<Civilizacion> Civilizaciones);
+
+    // Métodos de acceso
     void setNombre(const string &nom);
     string getNombre();
+
+    // Agregar
     void AgregarCiv(const Civilizacion &civ);
     void insertar(const Civilizacion &civ, size_t pos);
     void inicializar(const Civilizacion &civ, size_t n);
+
+    // Mostrar
     Civilizacion PrimerCiv();
     Civilizacion UltimaCiv();
+    void mostrar();
+
+    // Ordenar
     void OrdenarNom();
     void OrdenarUbX();
     void OrdenarUbY();
     void OrdenarPun();
+
+    // Eliminar
     void eliminar(const string &nom);
+
+    // Buscar
     Civilizacion* buscar(const Civilizacion &civ);
+
     size_t size();
-    void mostrar();
 };
 
 
