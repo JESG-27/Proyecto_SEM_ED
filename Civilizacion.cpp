393ed9fe10b391abcb16f22b0bcd7bd4cd1a81b1
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2021-11-17 14:12:21 -0600

    Comportamiento de cada método para aldenos

--- Civilizacion.cpp
+++ Civilizacion.cpp
@@ -50,3 +50,79 @@ float Civilizacion::getPuntuacion()
 {
     return puntuacion;
 }
+
+void Civilizacion::agregarFinal(const Aldeano &a)
+{
+    aldeanos.push_back(a);
+}
+
+void Civilizacion::agregarInicio(const Aldeano &a)
+{
+    aldeanos.push_front(a);
+}
+
+// Mostrar
+void Civilizacion::print()
+{
+    for (auto it = aldeanos.begin(); it != aldeanos.end(); it++)
+    {
+        cout << *it << endl;
+    }
+}
+
+// Eliminar
+void Civilizacion::eliminarNombre(const string &nombre)
+{
+    for (auto it = aldeanos.begin(); it != aldeanos.end(); it++)
+    {
+        Aldeano &a = *it;
+        if (a.getNombre() == nombre)
+        {
+            aldeanos.erase(it);
+            break;
+        }
+    }
+}
+
+void Civilizacion::elimnarSalud(const float &num)
+{
+    aldeanos.remove_if([num](const Aldeano &a){return a.getSalud() < num;});
+}
+
+void Civilizacion::eliminarEdad()
+{
+    aldeanos.remove_if([](const Aldeano &a){return a.getEdad() >= 60;});
+}
+
+// Ordenar
+void Civilizacion::ordenarNombre()
+{
+    aldeanos.sort();
+}
+
+void Civilizacion::ordenarEdad()
+{
+    aldeanos.sort([](const Aldeano &a1, const Aldeano &a2){return a1.getEdad() < a2.getEdad();});
+}
+
+void Civilizacion::ordenarSalud()
+{
+    aldeanos.sort([](const Aldeano &a1, const Aldeano &a2){return a1.getSalud() < a2.getSalud();});
+}
+
+// Buscar
+Aldeano* Civilizacion::buscarAldeano(const Aldeano &a1)
+{
+   for (auto it = aldeanos.begin(); it != aldeanos.end(); it++)
+    {
+        Aldeano a2 = *it;
+        if (a2.getNombre() == a1.getNombre())
+        {
+            return &(*it);
+        }
+        else if (it == aldeanos.end())
+        {
+            return nullptr;
+        }
+    }
+}
