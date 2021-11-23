3c42da91dffe9c2a503ecb7bd98e6f355fb61366
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2021-11-23 17:05:02 -0600

    Creación de la clase Guerrero

---
+++ Guerrero.h
@@ -0,0 +1,44 @@
+#ifndef GUERRERO_H
+#define GUERRERO_H
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Guerrero
+{
+private:
+    string id;
+    int salud;
+    float fuerza;
+    float escudo;
+    string tipo;
+
+public:
+    Guerrero(){};
+    Guerrero(const string &id, const int &salud, const float &fuerza, const float &escudo, const string &tipo);
+    
+    void setId(const string &ID);
+    string getId();
+    void setSalud(const float &SALUD);
+    float getSalud();
+    void setFuerza(const float &FUERZA);
+    float getFuerza();
+    void setEscudo(const float &ESCUDO);
+    float getEscudo();
+    void setTipo(const string &TIPO);
+    string getTipo();
+
+    friend ostream &operator<<(ostream &out, const Guerrero &g)
+    {
+        out << left;
+        out << setw(15) << g.id;
+        out << setw(15) << g.salud;
+        out << setw(15) << g.fuerza;
+        out << setw(15) << g.escudo;
+        out << setw(15) << g.tipo;
+    }
+
+};
+
+#endif
