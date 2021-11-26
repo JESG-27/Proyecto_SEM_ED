efd3bb7e80eda53c93255943efc6a3b497206582
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2021-11-25 23:04:02 -0600

    Menú para objetos tipo Barco

---
+++ menuBarco.h
@@ -0,0 +1,75 @@
+void capturarBarco(Civilizacion &c)
+{
+    Barco *b = new Barco();
+    string id;
+    b->setVelocidad(0.0);
+    b->setArmadura(100.0);
+    float combustible;
+
+    cout << "ID: ";
+    getline(cin, id);
+    b->setId(id);
+
+    cout << "Combustible: ";
+    cin >> combustible; cin.ignore();
+    b->setCombustible(combustible);
+
+    c.agregarBarco(b);
+}
+
+void menuBarcos(VideoGame &VG)
+{
+    string op;
+
+    while (true)
+    {
+        cout << "----- Barcos -----" << endl;
+        cout << "1) Agregar Barco" << endl; 
+        cout << "2) Mostrar Barcos" << endl; 
+        cout << "3) Buscar Barco" << endl; 
+        cout << "4) Eliminar Barco" << endl; 
+        cout << "0) Salir" << endl;
+        cout << ": ";
+        getline(cin, op);
+
+        if (op == "1")
+        {
+            Civilizacion search;
+            string nombre;
+            cout << "Nombre de civilizacion: ";
+            getline (cin, nombre);
+            search.setNombre(nombre);
+            Civilizacion *ptr = VG.buscar(search);
+            if (ptr == nullptr)
+            {
+                cout << "La busqueda no arrojo resultados" << endl;
+            }
+            else 
+            {
+                cout << "Civilizacion: " << ptr->getNombre() << endl;
+                cout << "Agregar Barco" << endl;
+                capturarBarco(*ptr);
+            }
+        } 
+
+        else if (op == "2")
+        {
+            VG.mostrarBarcos();
+        }
+
+        else if (op == "3")
+        {
+
+        }
+
+        else if (op == "4")
+        {
+
+        }
+        
+        else if (op == "0")
+        {
+            break;
+        }
+    }
+}
